a83fab7e2190a380b53d01b7533d7a9853ba21b2
tnotheis <timo.notheisen@js-soft.com>
2021-11-26 14:22:07 +0100

    ci: do not restore/build in publish.sh

--- .ci/publish.sh
+++ .ci/publish.sh
@@ -5,8 +5,6 @@ set -e
 
 ./.ci/find_changed_projects.sh | while read proj; do 
     proj_path="$proj/$proj"
-    dotnet restore $proj_path
-    dotnet build $proj_path --no-restore
     dotnet pack $proj_path --no-restore --no-build
     dotnet nuget push **/*.nupkg -k $NUGET_PUBLISHER_API_KEY -s https://api.nuget.org/v3/index.json
 done
