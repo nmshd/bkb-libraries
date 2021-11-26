95d74a134beca88f603f15e29f988c72ee6c5b39
tnotheis <timo.notheisen@js-soft.com>
2021-11-26 13:53:51 +0100

    ci: format test.sh

--- .ci/test.sh
+++ .ci/test.sh
@@ -3,6 +3,9 @@
 set -x
 set -e
 
-./.ci/find_changed_projects.sh | tee /dev/stderr | while read proj; do dotnet test "${proj}/${proj}.Tests"; done
+./.ci/find_changed_projects.sh | tee /dev/stderr | while read proj
+do 
+    dotnet test "${proj}/${proj}.Tests"; 
+done
 
 #--logger "junit;LogFilePath=test-results/results.xml"
