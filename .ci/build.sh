7a66eb6f05e190d6c6d207a5d78c1de9241b157d
tnotheis <timo.notheisen@js-soft.com>
2021-11-29 13:32:45 +0100

    chore: build with release configuration

--- .ci/build.sh
+++ .ci/build.sh
@@ -5,7 +5,7 @@ set -e
 
 ./.ci/find_changed_projects.sh | tee /dev/stderr | while read proj
 do 
-    project_path="$proj/$proj"
-    dotnet restore $project_path
-    dotnet build $project_path --no-restore
+    cd "$proj/$proj"
+    dotnet restore
+    dotnet build --configuration Release --no-restore
 done
