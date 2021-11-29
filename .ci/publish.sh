719cc0ac3343656b84dd3cf4eb5e19db420ba614
tnotheis <timo.notheisen@js-soft.com>
2021-11-29 08:44:04 +0100

    ci: pass nuget api key to publish script

--- .ci/publish.sh
+++ .ci/publish.sh
@@ -3,9 +3,9 @@
 set -x
 set -e
 
-./.ci/find_changed_projects.sh | while read proj; do 
-    proj_path="$proj/$proj"
-    dotnet pack $proj_path --no-restore --no-build
-    dotnet nuget push **/*.nupkg -k $NUGET_PUBLISHER_API_KEY -s https://api.nuget.org/v3/index.json
+./.ci/find_changed_projects.sh | while read project_name; do 
+    project_path="$project_name/$project_name"
+    dotnet pack $project_path --no-restore --no-build
+    dotnet nuget push **/*.nupkg -k $NUGET_API_KEY -s https://api.nuget.org/v3/index.json
 done
 
