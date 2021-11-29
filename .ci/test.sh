30a915cfea22065d3a2699a467f6495235dbb194
tnotheis <timo.notheisen@js-soft.com>
2021-11-29 15:08:38 +0100

    chore: rename some variables

--- .ci/test.sh
+++ .ci/test.sh
@@ -4,9 +4,9 @@ set -x
 set -e
 
 
-./.ci/find_changed_projects.sh | tee /dev/stderr | while read proj
+./.ci/find_changed_projects.sh | tee /dev/stderr | while read library_name
 do 
-    test_project_path="${proj}/${proj}.Tests/${proj}.Tests.csproj"
+    test_project_path="${library_name}/${library_name}.Tests/${library_name}.Tests.csproj"
 
     if [ -e "$test_project_path" ]; then # check if test project exists
         dotnet test --no-build --no-restore $test_project_path;
