fe0f43a77359c38d2a9ca9af383b733b304a2fe6
tnotheis <timo.notheisen@js-soft.com>
2021-11-29 15:11:07 +0100

    fix: remove commented out code

--- .ci/find_changed_projects.sh
+++ .ci/find_changed_projects.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-echo "x"
-echo "y"
-# git --no-pager diff --name-only HEAD^ | grep csproj\$ | while read -r line # find all csproj files in diff
-# do
-#     diff=$(git --no-pager diff -U0 --raw HEAD^ $line) # get diff for file
 
-#     if [[ $diff =~ \<Version\>.*\<\/Version\> ]]; then # if file has a version tag
-#         echo $line | cut -f1 -d"/" # return the filename
-#     fi
-# done
+git --no-pager diff --name-only HEAD^ | grep csproj\$ | while read -r line # find all csproj files in diff
+do
+    diff=$(git --no-pager diff -U0 --raw HEAD^ $line) # get diff for file
+
+    if [[ $diff =~ \<Version\>.*\<\/Version\> ]]; then # if file has a version tag
+        echo $line | cut -f1 -d"/" # return the filename
+    fi
+done
