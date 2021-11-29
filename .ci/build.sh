7605ce9e9e21ce3f340f92b1275e0d4a51059066
tnotheis <timo.notheisen@js-soft.com>
2021-11-29 15:20:21 +0100

    ci: unify scripts

--- .ci/build.sh
+++ .ci/build.sh
@@ -5,9 +5,8 @@ set -e
 
 root_dir=$(pwd)
 
-./.ci/find_changed_projects.sh | tee /dev/stderr | while read library_name
-do 
-    cd "$root_dir/${library_name}/${library_name}"
+./.ci/find_changed_projects.sh | tee /dev/stderr | while read library_name; do 
+    cd "${root_dir}/${library_name}/${library_name}"
     dotnet restore
     dotnet build --configuration Release --no-restore
 done
--- .ci/publish.sh
+++ .ci/publish.sh
@@ -4,7 +4,7 @@ set -e
 root_dir=$(pwd)
 
 ./.ci/find_changed_projects.sh | while read library_name; do 
-    cd "$root_dir/${library_name}/${library_name}"
+    cd "${root_dir}/${library_name}/${library_name}"
     dotnet pack --configuration Release --no-restore --no-build
     dotnet nuget push bin/Release/*.nupkg -k $NUGET_API_KEY -s https://api.nuget.org/v3/index.json
 done
