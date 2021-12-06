d6496f132d95a77b99dbd9e52103b8e9d91c09a4
Timo Notheisen <65653426+tnotheis@users.noreply.github.com>
2021-12-06 10:01:54 +0100

    Fix/utc date time converter debug assertion fails on convertion of nullable datetime (#9)
    * fix: add NullableUtcDateTimeConverter
    
    * chore: set version to 1.0.1

--- BuildingBlocks.API/BuildingBlocks.API/Mvc/JsonConverters/UtcDateTimeConverter.cs
+++ BuildingBlocks.API/BuildingBlocks.API/Mvc/JsonConverters/UtcDateTimeConverter.cs
@@ -5,18 +5,53 @@ using System.Text.Json.Serialization;
 
 namespace Enmeshed.BuildingBlocks.API.Mvc.JsonConverters
 {
-    public class UtcDateTimeConverter : JsonConverter<DateTime?>
+    public class UtcDateTimeConverter : JsonConverter<DateTime>
     {
+        internal const string DEFAULT_FORMAT = "yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fffZ";
         private readonly string _format;
 
-        public UtcDateTimeConverter(string format = "yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fffZ")
+        public UtcDateTimeConverter(string format = DEFAULT_FORMAT)
         {
             _format = format;
         }
 
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(DateTime);
+        }
+
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var stringValue = reader.GetString();
+
+            if (stringValue == null)
+                throw new Exception("Value cannot be null");
+
+            return DateTime.Parse(stringValue);
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToUniversalTime().ToString(_format));
+        }
+    }
+
+    public class NullableUtcDateTimeConverter : JsonConverter<DateTime?>
+    {
+        private readonly string _format;
+
+        public NullableUtcDateTimeConverter(string format = UtcDateTimeConverter.DEFAULT_FORMAT)
+        {
+            _format = format;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(DateTime?);
+        }
+
         public override DateTime? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            Debug.Assert(typeToConvert == typeof(DateTime));
             var stringValue = reader.GetString();
             return stringValue == null ? null : DateTime.Parse(stringValue);
         }
